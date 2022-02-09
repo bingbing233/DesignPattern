9f23db3bd1b4a2c12f2a3e63b9014a1bbd0a2af7
bingbing233 <1720756631@qq.com>
2022-02-09 14:36:27 +0800

    策略模式、观察者模式、装饰着模式、工厂模式、单例模式、命令模式、适配器模式

---
+++ src/main/kotlin/Adapter.kt
@@ -0,0 +1,66 @@
+/**
+ * 适配器模式
+ * 定义：将一个类的接口，转换成客户期望的另一个接口，适配器让原来不兼容的类可以合作无间
+ * 举例：将火鸡适配成鸟类
+ */
+
+//定义一个鸟的接口，鸟可以飞、会吱吱叫
+interface Bird{
+    fun fly()
+    fun squeak()
+}
+
+//定义一个火鸡的接口，火鸡火鸡只能飞一小段距离，咯咯叫
+interface Turkey{
+    fun fly()
+    fun goble()
+}
+
+//定义一只燕子
+class Swallow:Bird{
+    override fun fly() {
+        println("Swallow can fly with wings")
+    }
+
+    override fun squeak() {
+        println("squeak")
+    }
+}
+
+//定义一只火鸡
+class WildTurkey : Turkey{
+
+    override fun fly() {
+        println("turkey can only fly a short distance")
+    }
+
+    override fun goble() {
+        println("goble")
+    }
+}
+
+//定义一个火鸡适配器，将火鸡适配成鸟类
+class TurkeyAdapter(private val turkey: Turkey) : Bird{
+
+    override fun fly() {
+        turkey.fly()
+    }
+
+    override fun squeak() {
+        turkey.goble()
+    }
+
+}
+
+fun main(){
+    //现在我们需要大量的bird，但是bird不够了，只能用Turkey来凑数
+    //现在轮到适配器登场
+    val birds = ArrayList<Bird>()
+        birds.add(Swallow())
+        birds.add(Swallow())
+
+    val turkey = WildTurkey()
+    val turkeyAdapter = TurkeyAdapter(turkey)
+        birds.add(turkeyAdapter)
+}
+
