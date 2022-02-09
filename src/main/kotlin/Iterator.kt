a4bd8cdf1de77e7a34ffb5cf2e089baeccc9ba01
bingbing233 <1720756631@qq.com>
2022-02-09 16:38:23 +0800

    策略模式、观察者模式、装饰着模式、工厂模式、单例模式、命令模式、适配器模式、外观模式、模板方法模式、迭代器模式

--- src/main/kotlin/Iterator.kt
+++ src/main/kotlin/Iterator.kt
@@ -1,5 +1,6 @@
 /**
  * 迭代器模式
+ * 定义：提供一个方法顺序访问集合中的各个元素，而又不暴露其内部的表示。
  * 理解：就是把遍历封装起来
  */
 
@@ -11,13 +12,22 @@ interface IIterator<T> {
 
 data class Food(val name: String, val cost: Int)
 
-class FoodMenu : IIterator<Food> {
+//定义一个菜单，里面的食物是一个列表
+class FoodMenu {
     private val foods = ArrayList<Food>()
     private var currentIndex = 0
 
     fun addFood(food: Food) {
         foods.add(food)
     }
+
+    fun createIterator(): FoodMenuIterator {
+        return FoodMenuIterator(foods)
+    }
+}
+//实现一个菜单迭代器
+class FoodMenuIterator(private val foods: ArrayList<Food>) :IIterator<Food>{
+    private var currentIndex = 0
 
     override fun hasNext(): Boolean {
         return currentIndex < foods.size
@@ -34,6 +44,7 @@ class FoodMenu : IIterator<Food> {
     override fun remove(index:Int) {
         foods.removeAt(index)
     }
+
 }
 
 fun main(){
@@ -43,8 +54,9 @@ fun main(){
         foodMenu.addFood(Food("pork",30))
         foodMenu.addFood(Food("rice",2))
     //让我们来遍历一下菜单
-        while (foodMenu.hasNext()){
-            println(foodMenu.next()?.name)
+    val iterator = foodMenu.createIterator()
+        while (iterator.hasNext()){
+            println(iterator.next()?.name)
         }
 }
 
@@ -52,3 +64,5 @@ fun main(){
  * 其实jdk也提供了一个Iterator接口供我们使用
  * 集合类普遍都实现了collection和Iterator接口，collection提供了更多的多集合操作的方法
  */
+
+//todo: 组合模式
